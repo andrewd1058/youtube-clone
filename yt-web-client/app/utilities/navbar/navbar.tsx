05f1175b935fd1bd115887a43b6e9ba27ce9bac8
anon <71670269+Mythrix5@users.noreply.github.com>
2026-04-30 17:44:36 -0700

    Added firestore for storing user data, polished the UI, and added user authentication

--- yt-web-client/app/utilities/navbar/navbar.tsx
+++ yt-web-client/app/utilities/navbar/navbar.tsx
@@ -1,15 +1,34 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { User } from "firebase/auth";
+
 import styles from  "./navbar.module.css"
+import SignIn from "./sign-in";
 
 export default function Navbar() {
+    // Init user state
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedHelper((user) => {
+            setUser(user);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
     return (
         <nav className={styles.nav}>
             <Link href="/">
-                <Image width={90} height={20}
+                <Image width={110} height={30}
                     src="/youtube-logo.svg" alt="Youtube Logo" 
                 />
             </Link>
+            <SignIn user={user} />
         </nav>
     );
 }
---
+++ yt-web-client/app/utilities/navbar/sign-in.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import styles from "./sign-in.module.css";
+import { signInWithGoogle, signOut } from "../firebase/firebase";
+import { User } from "firebase/auth";
+
+interface SignInProps {
+    user: User | null;
+}
+
+export default function SignIn({ user }: SignInProps) {
+    return (
+        <>
+            { user ?
+                (
+                    <button className={styles.signin} onClick={signOut}>
+                        Sign Out
+                    </button>
+                ) : (
+                    <button className={styles.signin} onClick={signInWithGoogle}>
+                        Sign In
+                    </button>
+                )
+            }
+        </>
+    )
+}
